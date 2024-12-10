1ab0aa47219772cfcd0b86889d958ee9af9ce808
ritikco <ritikTyagi1312@gmail.com>
2024-12-10 15:37:23 +0530

    u

--- reactApp-main/frontend/src/component/AllProduct.js
+++ reactApp-main/frontend/src/component/AllProduct.js
@@ -6,7 +6,6 @@ import { useSelector } from "react-redux";
 const AllProduct = ({ heading }) => {
   const productData = useSelector((state) => state.product.productList);
   const categoryList = [...new Set(productData.map((el) => el.category))];
-  // console.log(categoryList);
   const [filterby, setFilterBy] = useState("");
   const [dataFilter, setDataFilter] = useState([]);
 
@@ -23,42 +22,44 @@ const AllProduct = ({ heading }) => {
       return [...filter];
     });
   };
+
   const loadingArrayFeature = new Array(10).fill(null);
+
   return (
-    <div className=" my-5">
-      <h2 className="font-bold text-2xl text-slate-800 mt-4">{heading}</h2>
-      <div className="flex gap-4 justify-center overflow-scroll scrollbar-none">
+    <div className="my-5 px-4 md:px-10">
+      <h2 className="font-bold text-2xl text-slate-800 mt-4 text-center md:text-left">
+        {heading}
+      </h2>
+      {/* Filter section */}
+      <div className="flex gap-4 justify-center overflow-x-scroll scrollbar-none py-4 md:justify-start">
         {categoryList[0] ? (
-          categoryList.map((el) => {
-            return (
-              <FilterProduct
-                category={el}
-                key={el}
-                isActive={el.toLowerCase() === filterby.toLowerCase()}
-                onClick={() => handleFilterProduct(el)}
-              />
-            );
-          })
+          categoryList.map((el) => (
+            <FilterProduct
+              category={el}
+              key={el}
+              isActive={el.toLowerCase() === filterby.toLowerCase()}
+              onClick={() => handleFilterProduct(el)}
+            />
+          ))
         ) : (
           <div className="min-h-[150px] flex justify-center items-center">
             <p>Loading...</p>
           </div>
         )}
       </div>
+      {/* Product card section */}
       <div className="flex flex-wrap justify-center gap-4 my-4">
         {dataFilter[0]
-          ? dataFilter.map((el) => {
-              return (
-                <CardFeature
-                  key={el._id}
-                  id={el._id}
-                  image={el.image}
-                  name={el.name}
-                  category={el.category}
-                  price={el.price}
-                />
-              );
-            })
+          ? dataFilter.map((el) => (
+              <CardFeature
+                key={el._id}
+                id={el._id}
+                image={el.image}
+                name={el.name}
+                category={el.category}
+                price={el.price}
+              />
+            ))
           : loadingArrayFeature.map((el, index) => (
               <CardFeature loading="Loading..." key={index + "allProduct"} />
             ))}
