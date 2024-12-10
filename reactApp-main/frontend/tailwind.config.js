eb8b258ae0ffbe0165c28576689169f73ec19156
ritikco <ritikTyagi1312@gmail.com>
2024-12-10 15:33:06 +0530

    o

--- reactApp-main/frontend/tailwind.config.js
+++ reactApp-main/frontend/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      scrollbar: ['none'],
+    },
   },
   plugins: [require("tailwind-scrollbar")],
 };
